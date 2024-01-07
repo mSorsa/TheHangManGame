7d4868d72b58c69a3cb5cfb27bd2b6c7446161a8
mSorsa <74288880+mSorsa@users.noreply.github.com>
2024-01-07 14:14:17 +0100

    Add files via upload

---
+++ src/Extensions/ApplicationExtensions.cs
@@ -0,0 +1,24 @@
+﻿namespace TheHangManGame.Extensions;
+
+public static class ApplicationExtensions
+{
+    public static void Configure(this IApplicationBuilder app, IWebHostEnvironment env)
+    {
+        app.UseSession();
+        
+        // Configure the HTTP request pipeline.
+        if (!env.IsDevelopment())
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
+
+        app.UseRouting();
+
+        app.UseAuthorization();
+    }
+}
---
+++ src/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,32 @@
+﻿using TheHangManGame.Logging;
+using TheHangManGame.Services;
+using TheHangManGame.Services.Interfaces;
+
+namespace TheHangManGame.Extensions;
+
+public static class DependencyInjectionExtensions
+{
+    public static IServiceCollection AddDependencyInjection(this IServiceCollection services)
+    {
+        services.AddHttpClient();
+        services.AddHttpContextAccessor();
+        services.AddScoped<IRandomWordService, RandomWordService>();
+        services.AddScoped<IGuessService, GuessService>();
+        services.AddTransient<IPerformanceLogger, PerformanceLogger>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddSessionWithCache(this IServiceCollection services)
+    {
+        services.AddDistributedMemoryCache();
+        services.AddSession(options =>
+        {
+            options.IdleTimeout = TimeSpan.FromSeconds(20);  // User has 20 seconds to make guesses
+            options.Cookie.HttpOnly = true;
+            options.Cookie.IsEssential = true;
+        });
+        
+        return services;
+    }
+}
